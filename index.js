ac9110306f2d9a52149f4b414e6535f6c077d105
Donna Cadiente <djanecad@hotmail.com>
2018-10-15 19:53:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const API_KEY = "a1cbc4b45d6e7126f02f6ae69308ceed"
 const main = document.getElementById("cityForm")
-cityForm.addEventListener('submit', handleFormSubmit())
 
 function handleFormSubmit(event) {
   //handle submit event
@@ -13,22 +12,28 @@ function handleFormSubmit(event) {
 function fetchCurrentWeather(city) {
   //fetch current weather based on city
   console.log("Im in fetchCurrentWeather")
-  let url ='https://api.openweathermap.org/data/2.5/weather?APPID=' + API_KEY + '&q' + city
-  console.log (url)
-  fetch(url).then((response) => {console.log(response)})
-  .then(json => console.log(json));
+  let url ='https://api.openweathermap.org/data/2.5/weather?APPID=' + API_KEY + '&q=' + city
+  console.log(url)
+  fetch(url).then(response => response.json()).then(json => displayCurrentWeather(json));
 }
 
 function displayCurrentWeather(json) {
   //render current weather data to the DOM using provided IDs and json from API
-  fetch('https://api.openweathermap.org/data/2.5/weather?') + API_KEY
-  .then(resp => resp.json())
-  .then(json => console.log(json));
+  //Current Temp - temperature.value, Low - temperature.min, High - temperature.max, Humidity - humidity.value, Cloud Cover - clouds.value 
+ console.log('This is the function: displayCurrentWeather')
+ console.log(json)
+ console.log(json['main']['temp'])
+ console.log(json['main']['temp_min'])
+ console.log(json['main']['temp_max'])
+ console.log(json['main']['humidity'])
+ console.log(json['clouds']['all'])
 }
 
 
 function fetchFiveDayForecast(city) {
   //fetch five day forecast data based on city
+  console.log('Im in the fetchFiveDayForecast')
+  fetch(url).then(response => response.json()).then(json => displayFiveDayForecast(json));
 }
 
 function displayFiveDayForecast(json) {
